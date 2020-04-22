0a144244f1f144ff753d26967b7221b6b7e29d1d
Markus Heiser <markus.heiser@darmarit.de>
2020-04-22 18:22:37 +0200

    [boilerplate] add setup.py
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""
+Searx plugins from The Green Web Foundation
+
+tgwf-searx-plugins
+  All results of searx queries that not hosted on a green infrastructure are
+  filtered
+
+"""
+
+from setuptools import setup
+
+GIT_URL='https://github.com/return42/tgwf-searx-plugins'
+
+setup(
+    name                = 'tgwf-searx-plugins'
+    , version           = '0.1'
+    , description       = 'The Green Web Foundation searx plugins'
+    , long_description  = __doc__
+    , url               =  GIT_URL
+    , author            = 'The Green Web Foundation'
+    , author_email      = 'chris@productscience.co.uk'
+    , project_urls      = {
+        "Code"              : GIT_URL
+        , "Issue tracker"   : GIT_URL + "/issues"
+    }
+    , license           = 'GNU Affero General Public License'
+    , zip_safe          = False
+    , install_requires  = """
+searx @ git+https://github.com/asciimoo/searx
+flask-babel
+"""
+    , py_modules        = [
+        'only_show_green_results'
+    ]
+    , entry_points      = {
+        'searx.plugins' : [
+            'tgwf.green-results = only_show_green_results'
+        ]
+    }
+)
